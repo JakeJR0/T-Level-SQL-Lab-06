c5e4210a1909d46f71c3ac52347eb80ebadcd0a7
Jake JR <1453194@stu.tmc.ac.uk>
2022-10-19 14:39:06 +0100

    Adding site_info.php

---
+++ includes/site_info.php
@@ -0,0 +1,9 @@
+<?php
+# Site Information
+define("SITE_NAME", "Enrolment Site");
+define("SITE_DESCRIPTION", "This is a course enrolment form made for the T-Level PHP Lab 03");
+
+# Site Details for meta tags
+define("SITE_URL", $_SERVER['HTTP_HOST']);
+define("SITE_COLOUR", "#a50000");
+?>
