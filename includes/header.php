e28db1cde10aa00e8b75625bcf5bb987eaba1e63
Jake JR <1453194@stu.tmc.ac.uk>
2022-10-19 14:39:21 +0100

    Adding header.php

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php include 'site_info.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo SITE_NAME. ' - '. PAGE_NAME; ?></title>
+        <!-- Meta Tags -->
+        <meta charset="UTF-8">
+        <meta name="title" content="<?php echo SITE_NAME; ?>">
+        <meta name="description" content="<?php echo SITE_DESCRIPTION; ?>">
+        <meta name="theme-color" content="<?php echo SITE_COLOUR; ?>">
+        <meta property="twitter:card" content="summary">
+        <meta property="twitter:title" content="<?php echo SITE_NAME; ?>">
+        <meta property="twitter:url" content="<?php echo SITE_URL; ?>">
+        <meta property="twitter:description" content="<?php echo SITE_DESCRIPTION; ?>">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="<?php echo SITE_URL; ?>">
+        <meta property="og:description" content="<?php echo SITE_DESCRIPTION; ?>">
+        <meta property="og:title" content="<?php echo SITE_NAME; ?>">
+
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="includes/main_style.css">
+    </head>
+</html>
