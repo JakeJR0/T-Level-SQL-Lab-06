d9083b03e6cfd63e45d346f83b66e7e8826ef803
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-10-28 15:17:39 +0100

    Changing login.php

--- login.php
+++ login.php
@@ -1,6 +1,55 @@
 <?php
+session_start();
+
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
+    header('Location: index.php');
+}
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    if (!isset($_POST['student_id'])) {
+        header('Location: login.php');
+        echo "Student ID not set";
+    } elseif (!isset($_POST['password'])) {
+        header('Location: login.php');
+        echo "Password not set";
+    }
+
+    $student_id = $_POST['student_id'];
+    $password = $_POST['password'];
+
+    $student_id = strip_tags($student_id);
+    $password = strip_tags($password);
+
+    require_once 'includes/storage.php';
+
+    $student_id = mysqli_real_escape_string($connection, $student_id);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $login_query = "
+        SELECT 
+            student.ID,
+            student.password,
+            student.first_name
+        FROM students AS student
+        WHERE ID = '$student_id'
+    ";
+
+    $login_result = mysqli_query($connection, $login_query);
+
+    if (!$login_result) {
+        die("Error: " . mysqli_error($connection));
+    }
+
+    $row = mysqli_fetch_assoc($login_result);
+
+    if (password_verify($password, $row['password'])) {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['student_id'] = $row['ID'];
+        $_SESSION['first_name'] = $row['first_name'];
+        header('Location: index.php');
+    } else {
+        header('Location: login.php');
+    }
     
 } else {
     DisplayLogin();
@@ -20,6 +69,7 @@ function DisplayLogin() {
         ?>
     </head>
     <body>
+        <?php include_once 'includes/navbar.php'; ?>
         <!-- Header -->
         <div class="text-center" style="margin-top: 50px;">
             <h1 class="display-4">Student Login</h1>
